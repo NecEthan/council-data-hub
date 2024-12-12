f64d7d872e7a4ddf7d9075373d990a3aa51ad0a8
ethan <ethanowicks9@gmail.com>
2024-12-12 16:50:23 +0000

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: '**', redirectTo: ''}
+    { path: '', loadChildren: () => import('./dashboard/dashboard.routes').then(mod => mod.DASHBOARD_ROUTES) },
 ];
