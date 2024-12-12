33b2f6dd8145a46e0c0a87e6cc48a4d5693d1c7e
ethan <ethanowicks9@gmail.com>
2024-12-12 16:48:07 +0000

    add

--- src/app/dashboard/components/dashboard.service.ts
+++ src/app/dashboard/components/dashboard.service.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environmentProd } from "../../../enironments/enironment.prod";
 
 
 @Injectable({
     providedIn: 'root' 
 })
-export class ScrapedService {
+export class ScrapedService{
+    public baseApi = environmentProd.apiBaseUrl;
 
     constructor(private http: HttpClient) {}
-
-    baseApi = 'https://council-data-hub-backend-production.up.railway.app'
 
     public getScrapedData() {
         return this.http.get<any>(`${this.baseApi}/scrape/all`)
---
+++ src/enironments/enironment.prod.ts
@@ -0,0 +1,5 @@
+
+export const environmentProd  = {
+    production: true,
+    apiBaseUrl: 'https://council-data-hub-backend-production.up.railway.app'
+}
